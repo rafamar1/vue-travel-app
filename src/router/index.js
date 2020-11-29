5875fe7af62bc34897dac4158309047b53c442e3
Rafael Marquez <rafael.marquez@bluetab.net>
2020-11-29 14:44:56 +0100

    Initial commit for a travel-app made in vue for testing Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -18,10 +18,43 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
+  },
+  {
+    path: "/details/:id n",
+    name: "DestinationDetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
+      )
   }
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "rafamar-active-link",
   routes
 });
 
---
+++ src/views/DestinationDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>{{ destination.name }}</h2>
+    <div class="destination-details">
+      <img
+        :src="require(`../assets/${destination.image}`)"
+        :alt="destination.name"
+      />
+      <p>{{ destination.description }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import store from "../../store.js";
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id
+    };
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        destination => destination.id === this.destinationId
+      );
+    }
+  }
+};
+</script>
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
