17ad58ed3fb8dede03ffb722220b274132ced682
Rafael Marquez <rafael.marquez@bluetab.net>
2020-11-29 22:36:25 +0100

    Added notFound component to catch 404 Exception caused by wrong routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "../../store.js";
 
 Vue.use(VueRouter);
 
@@ -38,7 +39,34 @@ const routes = [
             /* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails.vue"
           )
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: "notFound" });
+      }
+    }
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue")
   }
 ];
 
